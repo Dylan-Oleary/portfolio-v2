f3b95e5d7788828c4296590f35c74bb5e99b5803
Dylan O'Leary <dylanolearydev@gmail.com>
2025-01-05 18:07:45 -0500

    use chat

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,60 @@
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  SystemMessagePromptTemplate,
+} from "@langchain/core/prompts";
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
+import type { NextRequest } from "next/server";
+import { getPineconeVectorStore } from "~/app/lib";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const question = body.question;
+
+  const llm = new ChatOpenAI({
+    model: "gpt-4o-mini",
+    temperature: 0.1,
+    streaming: true,
+  });
+  const prompt = ChatPromptTemplate.fromMessages([
+    SystemMessagePromptTemplate.fromTemplate(
+      `You are to answer questions as a person named Dylan.
+         Assume you are being interviewed for a job and answer the questions courteously and professionally.
+         Your knowledge is limited only to the context provided.
+         Do not be overly creative.`
+    ),
+    SystemMessagePromptTemplate.fromTemplate("Context: {context}"),
+    HumanMessagePromptTemplate.fromTemplate("{question}"),
+  ]);
+
+  const vectorStore = await getPineconeVectorStore({
+    embeddings: new OpenAIEmbeddings({ model: "text-embedding-3-large" }),
+    indexName: process.env.PINECONE_INDEX!,
+  });
+  const retriever = vectorStore.asRetriever({ k: 5 });
+  const context = await retriever.invoke(question);
+  const message = await prompt.pipe(llm).stream({ context, question });
+
+  return new Response(
+    new ReadableStream({
+      async start(controller) {
+        try {
+          for await (const chunk of message) {
+            controller.enqueue(chunk.content);
+          }
+        } catch (error) {
+          controller.error(error);
+        } finally {
+          controller.close();
+        }
+      },
+    }),
+    {
+      headers: new Headers({
+        "Content-Type": "text/plain",
+        "Cache-Control": "no-cache, no-transform",
+        Connection: "keep-alive",
+      }),
+    }
+  );
+}
--- src/app/hooks/useChat.ts
+++ src/app/hooks/useChat.ts
@@ -1,3 +1,75 @@
-export function useChat() {
-  return;
+import {
+  type Dispatch,
+  type FormEvent,
+  type SetStateAction,
+  useState,
+} from "react";
+
+type ChatMessage = {
+  id: string;
+  role: "user" | "assistant";
+  message: string;
+};
+
+export type UseChatReturn = {
+  input: string;
+  messages: ChatMessage[];
+  setInput: Dispatch<SetStateAction<string>>;
+  onSubmit: (event?: FormEvent) => Promise<void>;
+};
+
+export function useChat(): UseChatReturn {
+  const [input, setInput] = useState<string>("");
+  const [messages, setMessages] = useState<ChatMessage[]>([
+    {
+      id: self.crypto.randomUUID(),
+      role: "assistant",
+      message: "Hello! How can I help you today?",
+    },
+  ]);
+
+  const onSubmit = async (event?: FormEvent) => {
+    event?.stopPropagation();
+    event?.preventDefault();
+
+    const updatedMessages: ChatMessage[] = [
+      ...messages,
+      { id: self.crypto.randomUUID(), role: "user", message: input },
+    ];
+
+    setMessages(updatedMessages);
+
+    const response = await fetch("/api/chat", {
+      method: "POST",
+      body: JSON.stringify({ question: input }),
+    });
+
+    const reader = response.body?.getReader();
+
+    if (!reader) {
+      console.error("Failed to get reader from response body");
+      return;
+    }
+
+    const chatMessage: ChatMessage = {
+      id: self.crypto.randomUUID(),
+      role: "assistant",
+      message: "",
+    };
+    const decoder = new TextDecoder();
+    let isReaderFinished = false;
+
+    while (!isReaderFinished) {
+      const { value, done } = await reader.read();
+
+      if (value) {
+        chatMessage.message += decoder.decode(value);
+        setMessages([...updatedMessages, chatMessage]);
+      }
+
+      isReaderFinished = done;
+    }
+  };
+
+  return { input, messages, onSubmit, setInput };
 }
